8dc0649ddbd758b2e8766e59761ca98d08a78c73
Eugene Sviat <oceaos@gmail.com>
2021-07-16 18:35:36 +0300

    heal gitignore store folder issue try#2

--- cmd/env.sh
+++ cmd/env.sh
@@ -3,18 +3,10 @@ export HTTP_ADDR=localhost:8080
 
 ### File upload settings
 
-# google cloud storage (for file content)
-#export GOOGLE_APPLICATION_CREDENTIALS=./serviceaccount.json
-#export GC_BUCKET=my-cool-bucket
 
 # path for local file storage
 export FILE_PATH=../../files
 
-### DB settings
-
-# Postgres settings
-#export PG_URL=postgres://postgres:postgres@localhost/test?sslmode=disable
-#export PG_MIGRATIONS_PATH=file://../../store/pg/migrations
 
 
 # Logger settings
